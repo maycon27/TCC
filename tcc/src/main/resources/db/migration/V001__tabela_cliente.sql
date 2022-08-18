f21527997fbdc17bd01d88f8c8ae7da3d7206113
Maycon Douglas <douglasmaycon27@hotmail.commaycon27>
2022-08-18 20:24:05 -0300

    criação das tabelas de estabelecimento e clientes

---
+++ tcc/src/main/resources/db/migration/V001__tabela_cliente.sql
@@ -0,0 +1,20 @@
+
+
+CREATE TABLE CLIENTES
+(
+    ID               SERIAL NOT NULL,
+    NOME             VARCHAR(150) NOT NULL,
+    CPF              VARCHAR(20) NOT NULL,
+    EMAIL            VARCHAR(150) NOT NULL,
+    TELEFONE         VARCHAR(14),
+    LOGRADOURO       VARCHAR(60),
+    NUMERO           VARCHAR(10),
+    COMPLEMENTO      VARCHAR(60),
+    BAIRRO           VARCHAR(60),
+    CEP              VARCHAR(8),
+    NOME_MUNICIPIO   VARCHAR(60),
+    UF               CHAR(2),
+    NOME_PAIS        VARCHAR(60),
+    DATA_NASCIMENTO  TIMESTAMP,
+    PRIMARY KEY (ID)
+);
