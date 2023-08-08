1f37bff8259beea2ce7af253474d3d92ad4bbc7a
maycon27 <mayconpalmeiras2013@hotmail.com>
2023-08-08 20:27:42 -0300

    mudanças

--- src/main/java/com/tcc/api/resources/cadastro/PublicResource.java
+++ src/main/java/com/tcc/api/resources/cadastro/PublicResource.java
@@ -2,22 +2,22 @@ package com.tcc.api.resources.cadastro;
 
 import com.tcc.api.dto.ClienteDTO;
 import com.tcc.api.dto.EstabelecimentoDTO;
+import com.tcc.api.dto.NomeDTO;
 import com.tcc.api.resources.swagger.cadastro.PublicSwagger;
 import com.tcc.doman.event.RecursoCriadoEvent;
 import com.tcc.doman.service.ClienteService;
 import com.tcc.doman.service.EstabelecimentoService;
+import com.tcc.doman.service.TipoEstabelecimentoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/public", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -27,6 +27,9 @@ public class PublicResource implements PublicSwagger {
     private ClienteService clienteService;
     @Autowired
     private EstabelecimentoService estabelecimentoService;
+
+    @Autowired
+    private TipoEstabelecimentoService tipoEstabelecimentoService;
     @Autowired
     private ApplicationEventPublisher publisher;
     @Override
@@ -43,6 +46,12 @@ public class PublicResource implements PublicSwagger {
         EstabelecimentoDTO estabelecimentoSalvo = estabelecimentoService.criar(dto);
         publisher.publishEvent(new RecursoCriadoEvent(this, response, dto.getId()));
         return ResponseEntity.status(HttpStatus.CREATED).body(estabelecimentoSalvo);
+    }
+
+    @Override
+    @GetMapping("/nome")
+    public List<NomeDTO> pesquisarPorNome(@RequestParam String filter) {
+        return tipoEstabelecimentoService.BuscarPorNome(filter);
     }
 
 }
--- src/main/java/com/tcc/api/resources/swagger/cadastro/PublicSwagger.java
+++ src/main/java/com/tcc/api/resources/swagger/cadastro/PublicSwagger.java
@@ -2,11 +2,13 @@ package com.tcc.api.resources.swagger.cadastro;
 
 import com.tcc.api.dto.ClienteDTO;
 import com.tcc.api.dto.EstabelecimentoDTO;
+import com.tcc.api.dto.NomeDTO;
 import com.tcc.api.dto.ProdutoDTO;
 import io.swagger.annotations.*;
 import org.springframework.http.ResponseEntity;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @Api(tags = "End Points Publicos")
 public interface PublicSwagger {
@@ -26,4 +28,7 @@ public interface PublicSwagger {
     ResponseEntity<EstabelecimentoDTO> salvarEstabelecimento(
             @ApiParam(name = "corpo", value = "Representação de um novo Estabelecimento", required = true) EstabelecimentoDTO dto,
             HttpServletResponse response);
+
+    @ApiOperation("Pesquisa todos os Tipos Estabelecimentos que contenha uma parte ou nome")
+    List<NomeDTO> pesquisarPorNome(@ApiParam(value = "nome", example = "venda", required = true) String filter);
 }
